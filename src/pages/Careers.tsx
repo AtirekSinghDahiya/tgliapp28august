239fed829fd93261fd2fa3475ac162775205ee3a
AtirekSinghDahiya <aistearunica@gmail.com>
2025-08-29 13:31:02 -0400

    Fix motion import error in Careers component

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
 import { MapPin, Clock, DollarSign, Briefcase, Mail } from 'lucide-react'
 import { getJobPostings } from '../services/supabase'
 
